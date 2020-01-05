035cb8d15c69ce9e3af2c5b4233d324f7dac1cf8
ATiSh Maharjan <arcrt1993@gmail.com>
2020-01-05 19:36:24 +0545

    added menu-item and directory

---
+++ src/pages/homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+import Directory from '../directory/directory';
+import '../styles/homepage.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+       <Directory/>
+    </div>
+);
+
+export default HomePage;
