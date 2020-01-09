759a312ae52097ceeba761772ed2797970b66cfb
ATiSh Maharjan <arcrt1993@gmail.com>
2020-01-09 17:49:58 +0545

    signin form created"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import HomePage from './pages/homepage';
 import ShopPage from './pages/shop/shoppage';
+import Header from './components/header/header';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up';
 import './App.css';
 import {Route, Switch} from 'react-router-dom';
 
@@ -13,10 +15,12 @@ const HatsPage = () =>(
 function App() {
   return (
     <div>
-      <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route paths='/shop' component={ShopPage}/>
-      </Switch>
+      <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/shop' component={ShopPage}/>
+          <Route path='/sign-in' component={SignInSignUpPage}/>
+        </Switch>
     </div>
   );
 }
---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignIn from "../../components/sign-in/sign-in";
+import '../../styles/form-input.scss';
+
+const SignInSignUpPage = () =>(
+    <div className='sign-in-and-sign-up'>
+        <SignIn/>
+    </div>
+)
+
+export default SignInSignUpPage;
